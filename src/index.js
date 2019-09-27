9198dca3e098a205aa95b371c827f4f6d7ab82fd
waivey <waivey@gmail.com>
2019-09-27 09:54:30 +0100

    added css layout of board and classes for board and square

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import './index.css';
+
+class Board extends React.Component {
+    renderSquare(i) {
+        return <Square value={i} />
+    }
+}
