0fb22200deaa76c0e889d25efb34fb144c1d50f9
waivey <waivey@gmail.com>
2019-09-27 10:23:24 +0100

    onClick with placeholder func added to Square

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 class Square extends React.Component {
     render() {
         return (
-            <button className='square'>{this.props.value}</button>
+            <button className='square' onClick={function() { alert('click');}}>{this.props.value}</button>
         );
     }
 }
