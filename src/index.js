67537f3cf84ae3023b817f7e852991a21c4637c1
waivey <waivey@gmail.com>
2019-09-27 11:20:09 +0100

    func to handle xo switch added

--- src/index.js
+++ src/index.js
@@ -2,22 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: null,
-        };
-    }
-    
-    render() {
-        return (
-            <button className='square' 
-                    onClick={() => this.setState({value: 'X'})}>
-                    {this.state.value}
-            </button>
-        );
-    }
+function Square(props) {
+    return (
+        <button className='square' 
+                onClick={props.onClick}>
+                {props.value}
+        </button>
+    );  
 }
 
 class Board extends React.Component {
@@ -25,7 +16,17 @@ class Board extends React.Component {
         super(props);
         this.state = {
             squares: Array(9).fill(null),
+            xIsNext: true,
         };
+    }
+
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        });
     }
     
     renderSquare(i) {
@@ -38,7 +39,7 @@ class Board extends React.Component {
     }
 
     render() {
-        const status = 'Next player: X';
+        const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
         return (
             <div>
